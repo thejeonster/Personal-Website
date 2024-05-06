249caadffffeceb917a855311da6d50613256658
Robert <junhojeon010@gmail.com>
2024-05-05 18:16:25 -0600

    Testing

---
+++ main.js
@@ -0,0 +1,12 @@
+const titleHeader = document.querySelector("h1")
+titleHeader.style.textAlign = "center";
+titleHeader.style.color = "black";
+titleHeader.style.fontSize = "40px";
+titleHeader.style.fontFamily = "Georgia, Times New Roman, Times, serif";
+
+
+const headerOne = document.createElement("h2");
+headerOne.classList.add("headerOne");
+headerOne.textContent = "About Me"
+headerOne.style.color = "blue";
+titleHeader.appendChild(headerOne)
