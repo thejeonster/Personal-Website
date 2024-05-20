d4f269e8c5160e1bea9c9cec8a0ea1ffb4067f67
Robert <junhojeon010@gmail.com>
2024-05-20 17:23:43 -0600

    Added container for second layer

--- main.js
+++ main.js
@@ -9,3 +9,11 @@ layerOne.style.background = "#1D669E";
 
 document.body.appendChild(layerOne);
 
+// ----- Second Layer ----- // 
+
+const layerTwo = document.createElement("div"); 
+layerTwo.style.display = "flex"; 
+layerTwo.style.height = "350px";
+layerTwo.style.background = "#D3E6F5";
+
+document.body.appendChild(layerTwo);
