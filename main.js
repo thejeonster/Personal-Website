b151ca3864f1228ea1ce37b690798a8d4a0fde1d
Robert <junhojeon010@gmail.com>
2024-06-05 21:08:47 -0600

    Added typing effect to title

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 
 const titleHeader = document.createElement("h1");
+titleHeader.classList.add("title");
 titleHeader.textContent = "With Regards";
-
 document.body.appendChild(titleHeader);
+
+function titleTypingEffect(element, text, i = 0) {
+
+    element.textContent += text[i];
+
+    if (i < text.length - 1) {
+        setTimeout(() => titleTypingEffect(element, text, i + 1), 70);
+    }
+}
+
+titleHeader.textContent = "";
+titleTypingEffect(titleHeader, "Responsive Designs..."); 
